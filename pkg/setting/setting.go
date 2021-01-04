675229877ca6050fc9271df56e0265dc6c6bacc0
Redbo He <redbo.he@gshopper.com>
2021-01-04 20:17:31 +0800

    同步第一章节代码

---
+++ pkg/setting/setting.go
@@ -0,0 +1,56 @@
+package setting
+
+import (
+	"github.com/go-ini/ini"
+	"log"
+	"time"
+)
+
+var (
+	Cfg *ini.File
+
+	RunMode string
+
+	HttpPort int
+	ReadTimeout time.Duration
+	WriteTimeout time.Duration
+
+	PageSize int
+	JwtSecret string
+)
+
+func init() {
+	var err error
+	Cfg, err := ini.Load("conf/app.ini")
+	if err != nil {
+		log.Fatalf("Fail to parse 'conf/app.ini: %v", err)
+	}
+	LoadBase(Cfg)
+	LoadServer(Cfg)
+	LoadApp(Cfg)
+}
+
+func LoadBase(Cfg *ini.File) {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+
+func LoadServer(Cfg *ini.File) {
+	sec , err := Cfg.GetSection("server")
+	if err != nil {
+		log.Fatalf("Fail to get section 'server': %v", err)
+	}
+
+	HttpPort = sec.Key("HTTP_PORT").MustInt(8000)
+	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60 ) )* time.Second
+	WriteTimeout  = time.Duration(sec.Key("WRITE_TIMEOUT").MustInt(60 ) )* time.Second
+}
+
+func LoadApp(Cfg *ini.File) {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatalf("Fail to get section 'app': %v", err)
+	}
+
+	JwtSecret = sec.Key("JWT_SECRET").MustString("!@)*#)!@U#@*!@!)")
+	PageSize = sec.Key("PAGE_SIZE").MustInt(10)
+}
