0309dde121176ee5f887662d0251520d03aab48f
Redbo He <redbo.he@gshopper.com>
2021-01-05 16:44:47 +0800

    新增文章模块 增删改查

--- routers/router.go
+++ routers/router.go
@@ -16,13 +16,24 @@ func InitRouter() *gin.Engine {
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
-
 		//新建标签
 		apiv1.POST("/tags", v1.AddTag)
 		//更新指定标签
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		//删除指定标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
