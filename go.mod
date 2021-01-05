9532f87e68ac70acfe37cb07d1dba2951d8d788d
Redbo He <redbo.he@gshopper.com>
2021-01-05 20:31:26 +0800

    同步代码

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/astaxie/beego v1.12.3
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.62.0
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
--- main.go
+++ main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
+	"github.com/fvbock/endless"
 	"net/http"
 	"fmt"
 )
 
 func main() {
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	//endpoint := fmt.Sprintf("%d", setting.HttpPort)
+
 	router := routers.InitRouter()
-	s := &http.Server{
+	server := &http.Server{
 		Addr : fmt.Sprintf(":%d", setting.HttpPort),
 		Handler: router,
 		ReadTimeout: setting.ReadTimeout,
 		WriteTimeout: setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+	server.ListenAndServe()
 
 }
