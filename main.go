ce2b3cd415c016711411caea696c6adb72ee3b37
hehongbo <hhbjkd@163.com>
2021-01-07 01:27:51 +0800

    增加unix  热启动代码

--- main.go
+++ main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
 	"github.com/fvbock/endless"
-	"net/http"
-	"fmt"
+	"log"
+	"syscall"
 )
 
 func main() {
 	endless.DefaultReadTimeOut = setting.ReadTimeout
 	endless.DefaultWriteTimeOut = setting.WriteTimeout
 	endless.DefaultMaxHeaderBytes = 1 << 20
-	//endpoint := fmt.Sprintf("%d", setting.HttpPort)
+	endpoint := fmt.Sprintf(":%d", setting.HttpPort)
 
 	router := routers.InitRouter()
-	server := &http.Server{
-		Addr : fmt.Sprintf(":%d", setting.HttpPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+
+	server := endless.NewServer(endpoint, router)
+	server.BeforeBegin = func(add string ) {
+		log.Printf("Actual pid is %d", syscall.Getpid() )
 	}
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+	
+	//server := &http.Server{
+	//	Addr : fmt.Sprintf(":%d", setting.HttpPort),
+	//	Handler: router,
+	//	ReadTimeout: setting.ReadTimeout,
+	//	WriteTimeout: setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//server.ListenAndServe()
 
 }
